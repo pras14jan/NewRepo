e3c814218c5f8acfe981adbcff679863d45fc7ed
Prashant <pras14jan@gmail.com>
2019-03-28 01:12:20 +0530

    upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        var catalog = new Catalog();
+        catalog.Add(Divisor: 15, Output: "FizzBuzz");
+        catalog.Add(Divisor: 3, Output: "Fizz");
+        catalog.Add(Divisor: 5, Output: "Buzz");
+
+        var counter = new Counter(Min: 1, Max: 100, Catalog: catalog);
+        counter.Output();
+        Console.ReadLine();
+    }
+}
+
+internal class Counter
+{
+    private int min;
+    private int max;
+    private Catalog catalog;
+
+    internal Counter(int Min, int Max, Catalog Catalog)
+    {
+        this.min = Min;
+        this.max = Max;
+        this.catalog = Catalog;
+    }
+    internal void Output()
+    {
+        for (int i = min; i <= max; i++)
+        {
+            Console.WriteLine(catalog.ToString(i));
+        }
+    }
+}
+
+internal class Catalog
+{
+    private List<Spec> specs;
+    internal class Spec
+    {
+        internal int divisor;
+        internal string output;
+    }
+    internal Catalog()
+    {
+        this.specs = new List<Spec>();
+    }
+    internal void Add(int Divisor, string Output)
+    {
+        this.specs.Add(new Spec() { divisor = Divisor, output = Output });
+    }
+    internal string ToString(int Number)
+    {
+        string outputstring = "";
+        foreach (var x in specs)
+        {
+            outputstring += Number % x.divisor == 0 ? x.output : "";
+        }
+        return String.IsNullOrWhiteSpace(outputstring) ? Number.ToString() : outputstring;
+    }
+}
---
+++ Program1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            var generators = new Func<string, int, string>[]
+            {
+        (s, i) => i % 3 == 0 ? s + "Fizz" : s,
+        (s, i) => i % 5 == 0 ? s + "Buzz" : s,
+        (s, i) => s ?? i.ToString()
+            };
+            var results = Enumerable.Range(1, 100).Select(i => generators.Aggregate((string)null, (s, f) => f(s, i))).ToList();
+            results.ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+    }
+}
